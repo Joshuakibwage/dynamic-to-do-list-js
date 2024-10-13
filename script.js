cd3c8a7d6c274a84314ef80e8fabac8bad8f741c
Josh <joshuakibwage23@gmail.com>
2024-10-13 08:39:25 +0300

    Add event listener for page load

---
+++ script.js
@@ -0,0 +1,9 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const addButton = document.getElementById('add-task-btn')
+    const taskInput = document.getElementById('task-input')
+    const taskList = document.getElementByiD('task-list')
+
+ 
+});
+
