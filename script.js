e433895108e4ab2a02d3fa3319353937b02e4230
Josh <joshuakibwage23@gmail.com>
2024-10-13 08:52:02 +0300

    Add create task funcion

--- script.js
+++ script.js
@@ -3,7 +3,16 @@ document.addEventListener('DOMContentLoaded', function() {
     const addButton = document.getElementById('add-task-btn')
     const taskInput = document.getElementById('task-input')
     const taskList = document.getElementByiD('task-list')
-
+      
+    function addTask(){
+        
+        let taskTest = taskInput.value.trim();
+        if(taskTest === ''){
+            alert('Enter a task!');
+            return;
+        }
+    
+    }
  
 });
 
