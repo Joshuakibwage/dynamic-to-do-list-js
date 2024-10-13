9ba4a6539cc34f5f7c8e261c84548bfd82f62926
Josh <joshuakibwage23@gmail.com>
2024-10-13 12:38:22 +0300

    Add remove button

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ document.addEventListener('DOMContentLoaded', function() {
             taskList.removeChild(task);
             
         }
+        taskList.appendChild(rmvButton);
     }
  
 });
